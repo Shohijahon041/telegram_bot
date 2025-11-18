c427dfdbd72b723c408e4a8fa98005c6fcf40927
Shohijahon_041 <142481446+Shohijahon041@users.noreply.github.com>
2025-11-18 12:42:56 +0500

    Add comments to configuration parameters
    Added comments for better understanding of configuration parameters.

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
+# Bot konfiguratsiyasi
 TOKEN = "8353415660:AAGNHYsJ50H8wm7gmBJPKyO0QOvUcyufKqY"
 ADMIN_ID = 5453333583
-MOVIE_CHANNEL_ID = "-1003134037650"
-INSTAGRAM_LINK = "https://instagram.com/your_profile"
+MOVIE_CHANNEL_ID = "-1003134037650"  # Maxfiy kanal ID
+INSTAGRAM_LINK = "https://instagram.com/your_profile"  # Keyin admin paneldan o'zgartirishingiz mumkin
