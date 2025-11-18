d4a67def3751284aa8f27bb1e4f67353f911dc26
Shohijahon_041 <142481446+Shohijahon041@users.noreply.github.com>
2025-11-18 12:46:02 +0500

    Update comments in config.py for clarity

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-# Bot konfiguratsiyasi
+# config.py — bot sozlamalari
 TOKEN = "8353415660:AAGNHYsJ50H8wm7gmBJPKyO0QOvUcyufKqY"
 ADMIN_ID = 5453333583
-MOVIE_CHANNEL_ID = "-1003134037650"  # Maxfiy kanal ID
-INSTAGRAM_LINK = "https://instagram.com/your_profile"  # Keyin admin paneldan o'zgartirishingiz mumkin
+MOVIE_CHANNEL_ID = "-1003134037650"  # maxfiy kino kanalining IDsi
+INSTAGRAM_LINK = "https://instagram.com/your_profile"  # keyin admin paneldan o‘zgartirish mumkin
